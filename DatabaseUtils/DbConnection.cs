ac506c1669bb9224c3e0aba48673573494b65cb9
lnvictor <lnvictor@github.com>
2023-11-09 00:55:09 -0300

    Inserting DBConnection layer

---
+++ DatabaseUtils/DbConnection.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+
+namespace DatabaseUtils
+{
+    public class DbConnection
+    {
+        private static DbConnection? connection;
+        private NpgsqlConnection? npgsqlConnection;
+        private string connectionString;
+
+        private DbConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public static DbConnection GetInstance(string connectionString)
+        {
+            if (connection == null)
+            {
+                connection = new DbConnection(connectionString);
+            }
+            return connection;
+        }
+
+        public NpgsqlConnection GetConnection() {
+            if (npgsqlConnection == null)
+            {
+                npgsqlConnection = new NpgsqlConnection(connectionString);
+                npgsqlConnection.Open();
+            }
+            return npgsqlConnection;
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                if (npgsqlConnection != null)
+                {
+                    npgsqlConnection.Close();
+                    npgsqlConnection.Dispose();
+                    npgsqlConnection = null;
+                }
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
