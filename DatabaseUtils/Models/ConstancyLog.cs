3e6a7cdcb304a1bab3a3ba6717976a544e466527
lnvictor <lnvictor@github.com>
2023-11-09 00:54:28 -0300

    Adding ConstancyLogs Model

---
+++ DatabaseUtils/Models/ConstancyLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseUtils.Models
+{
+    public class ConstancyLog
+    {
+        public readonly Boolean Study;
+        public readonly Boolean Exercise;
+        public readonly Boolean WorkFocused;
+
+        public ConstancyLog(Boolean study, Boolean exercise, Boolean workFocused) {
+            Study = study;
+            Exercise = exercise;
+            WorkFocused = workFocused;
+        }
+    }
+}
