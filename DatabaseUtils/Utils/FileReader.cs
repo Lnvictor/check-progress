003854a7814cdb91fed356d6c07bedff3ebf290e
lnvictor <lnvictor@github.com>
2023-11-09 01:05:30 -0300

    updating FileReader

---
+++ DatabaseUtils/Utils/FileReader.cs
@@ -0,0 +1,32 @@
+﻿using DatabaseUtils.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseUtils.Utils
+{
+    public  class FileReader
+    {
+        private const string FilesBasePath = "C:\\Check_Progress";
+        public static ConstancyLog ReadDayFromFile(DateTime dateTime)
+        {
+            string fullPath = $"{FilesBasePath}\\{dateTime.Year}{dateTime.Month}{dateTime.Day}.txt";
+            if (!File.Exists(fullPath))
+            {
+                return new ConstancyLog(false, false, false);
+            }
+
+            using (StreamReader reader = new StreamReader(fullPath))
+            {
+                string[] lines = reader.ReadToEnd().Split("\n");
+                bool exercise = Boolean.Parse(lines[0]);
+                bool study = Boolean.Parse(lines[1]);
+                bool workFocused = Boolean.Parse(lines[2]);
+
+                return new ConstancyLog(study, exercise, workFocused);
+            }
+        }
+    }
+}
